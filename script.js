374cb4725364f4adf635b33c69983bbe265a700b
heisumesh2006 <152083579+heisumesh2006@users.noreply.github.com>
2024-05-19 06:34:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// URLs of the websites you want to combine
+const url1 = 'file:///C:/Users/umesh/Desktop/umesh/Clash-Of-Space/index.html';
+const url2 = 'file:///C:/Users/umesh/Downloads/index.html';
+
+// Set the src attribute of the iframes to the URLs
+document.getElementById('iframe1').src = url1;
+document.getElementById('iframe2').src = url2;
+
+"C:\Users\umesh\Downloads\no_of_lifes.PNG"
